9cae51ce3ede52cfb1fbd93ca623ac80b87bfbc8
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-07-09 23:58:28 -0400

    Terminando la entidad Clientes

---
+++ Entidades/Clientes.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System;
+
+namespace Entidades
+{
+    public class Clientes
+    {
+        [Key]
+        public int ClienteId { get; set; }
+        public string Nombre { get; set; }
+        public string Celular { get; set; }
+        public string Telefono { get; set; }
+        public string Email { get; set; }
+        public string  Direccion { get; set; }
+        public DateTime FechaCreacion{ get; set; }
+        public int UsuarioId { get; set; }
+
+        public Clientes()
+        {
+            ClienteId = 0;
+            Nombre = string.Empty;
+            Celular = string.Empty;
+            Telefono = string.Empty;
+            Email = string.Empty;
+            Direccion = string.Empty;
+            FechaCreacion = DateTime.Now;
+            UsuarioId = 0;
+        }
+    }
+}
