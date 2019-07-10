91b8a5b6522ed9e5996514897bc46e1a03c96e8b
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-07-10 00:05:36 -0400

    Terminando la entidad TiposTrabajos

--- Entidades/TiposTrabajos.cs
+++ Entidades/TiposTrabajos.cs
@@ -1,12 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entidades
 {
-    class TiposTrabajos
+    public class TiposTrabajos
     {
+        [Key]
+        public int TipoTrabajoId { get; set; }
+        public string Descripcion { get; set; }
+        public DateTime FechaCreacion { get; set; }
+        public int UsuarioId { get; set; }
+
+        public TiposTrabajos()
+        {
+            TipoTrabajoId = 0;
+            Descripcion = string.Empty;
+            FechaCreacion = DateTime.Now;
+            UsuarioId = 0;
+        }
     }
 }
