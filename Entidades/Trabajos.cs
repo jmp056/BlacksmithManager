492b03814ef886c1a9abe88097b96703d27d228b
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-07-10 00:30:59 -0400

    Terminando de crear la entidad Trabajos

--- Entidades/Trabajos.cs
+++ Entidades/Trabajos.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
 
@@ -9,6 +10,42 @@ namespace Entidades
         [Key]
         public int TrabajoId { get; set; }
         public DateTime FechaInicio { get; set; }
+        public string Cliente { get; set; }
+        public string TipoTrabajo { get; set; }
+        public string Descripcion { get; set; }
+        public string Direccion { get; set; }
+        public decimal Precio { get; set; }
+        public string Encargado { get; set; }
+        public decimal Ajuste { get; set; }
+        public virtual List<Movimientos> Movimientos { get; set; }
+        public decimal Cobrado { get; set; }
+        public decimal Balance { get; set; }
+        public decimal AjustePagado { get; set; }
+        public decimal AjustePendiente { get; set; }
+        public decimal Materiales { get; set; }
+        public decimal GananciaBruta { get; set; }
+        public decimal GananciaNeta { get; set; }
 
+        public Trabajos()
+        {
+            TrabajoId = 0;
+            FechaInicio = DateTime.Now;
+            Cliente = string.Empty;
+            TipoTrabajo = string.Empty;
+            Descripcion = string.Empty;
+            Direccion = String.Empty;
+            Precio = 0;
+            Encargado = String.Empty;
+            Ajuste = 0;
+            Movimientos = new List<Movimientos>();
+            Cobrado = 0;
+            Balance = 0;
+            AjustePagado = 0;
+            AjustePendiente = 0;
+            Materiales = 0;
+            GananciaBruta = 0;
+            GananciaNeta = 0;
+        }
     }
+
 }
