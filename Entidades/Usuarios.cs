9ad9acb83323e40cdb929dd34a38f7afd2ea420a
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-07-10 00:08:51 -0400

    Terminando la entidad Usuarios

--- Entidades/Usuarios.cs
+++ Entidades/Usuarios.cs
@@ -1,12 +1,32 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Entidades
 {
-    class Usuarios
+    public class Usuarios
     {
+        [Key]
+        public int UsuarioId { get; set; }
+        public string Nombres { get; set; }
+        public string Email { get; set; }
+        public int NivelUsuario { get; set; }
+        public string Usuario { get; set; }
+        public string Clave { get; set; }
+        public DateTime FechaIngreso { get; set; }
+
+        public Usuarios()
+        {
+            UsuarioId = 0;
+            Nombres = string.Empty;
+            Email = string.Empty;
+            NivelUsuario = 0;
+            Usuario = string.Empty;
+            Clave = string.Empty;
+            FechaIngreso = DateTime.Now;
+        }
     }
 }
