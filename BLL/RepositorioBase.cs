158c876a9001d00a85689614029f984ade02674e
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-07-10 23:16:46 -0400

    Terminando el Contexto y agregando la entidad Usuarios a el

--- BLL/RepositorioBase.cs
+++ BLL/RepositorioBase.cs
@@ -1,10 +1,9 @@
-﻿using System;
+﻿using DAL;
+using System;
 using System.Collections.Generic;
 using System.Data.Entity;
 using System.Linq;
 using System.Linq.Expressions;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace BLL
 {
---
+++ DAL/Contexto.cs
@@ -0,0 +1,12 @@
+﻿using Entidades;
+using System.Data.Entity;
+
+namespace DAL
+{
+    public class Contexto : DbContext
+    {
+        public DbSet<Usuarios> Usuarios { get; set; }
+        public Contexto() : base("ConStr")
+        { }
+    }
+}
