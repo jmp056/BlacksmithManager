cc1daca6d46d2137f53877d5cbd20533c49c662d
Jonathan Maria Parra <jonathan_maria@ucne.edu.do>
2019-07-28 16:41:36 -0400

    Creando el test

---
+++ BLLTests/RepositorioBaseTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BLL.Tests
+{
+    [TestClass()]
+    public class RepositorioBaseTests
+    {
+        [TestMethod()]
+        public void GuardarTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
---
+++ EntidadesTests/ClientesTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Entidades;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades.Tests
+{
+    [TestClass()]
+    public class ClientesTests
+    {
+        [TestMethod()]
+        public void ClientesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
